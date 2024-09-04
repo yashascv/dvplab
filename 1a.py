608704286bc0700639766bf31e6e9be1b4b20248
yashascv <yashascv67@gmail.com>
2024-09-04 12:34:53 +0530

    Add files via upload

---
+++ 1a.py
@@ -0,0 +1,10 @@
+m1=int(input("enter a number 1:"))
+m2=int(input("enter a number 2:"))
+m3=int(input("enter a number 3:"))
+if(m1<m2 and m1<m3):
+    Avg=(m2+m3)/2
+elif(m2<m1 and m2,m3):
+    Avg=(m1+m3)/2
+elif(m3<m1 and m3<m2)/2:
+    Avg=(m1+m2)/2
+print("Average of 2 numbers",Avg)
