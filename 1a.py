18bd02445b8f65d637616894cbe6c58d338ad762
yashascv <yashascv67@gmail.com>
2024-09-24 19:38:28 +0530

    Update 1a.py

--- 1a.py
+++ 1a.py
@@ -3,7 +3,7 @@ m2=int(input("enter a number 2:"))
 m3=int(input("enter a number 3:"))
 if(m1<m2 and m1<m3):
     Avg=(m2+m3)/2
-elif(m2<m1 and m2,m3):
+elif(m2<m1 and m2<m3):
     Avg=(m1+m3)/2
 else:
     Avg=(m1+m2)/2
