cfbc258774189018eadaf2e7cf3d12f1b2960402
yashascv <yashascv67@gmail.com>
2024-10-16 12:57:04 +0530

    Add files via upload

---
+++ 5a.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.hist([45,55,67,88,24,37,41,29,90,35,28,77,68],bins=4)
+plt.xlabel("Distribution of age",fontsize=16)
+plt.ylabel("Frequency",fontsize=16)
+plt.title("Yashas-1KI23CS185")
+plt.show()
---
+++ 5b.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+CARS=['range rover','benz','tata nexon','venue','duster','bmw']
+data=['50','45','10','20','5','4']
+plt.figure(figsize=(10,7))
+plt.pie(data,labels=CARS)
+plt.title("Yashas-1KI23CS185")
+plt.show()
