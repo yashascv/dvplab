4bbdd168dfdb2db5ff503314109e2f54ca0d9b0e
yashascv <yashascv67@gmail.com>
2024-11-13 12:53:07 +0530

    Add files via upload

--- 7a.py
+++ 7a.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-def sinplot(n=5):
-    x=np.linspace(0,20,10)
+def sinplot(n=10):
+    x=np.linspace(0,20,50)
     for i in range(1,n+1):
         plt.plot(x,np.sin(x+i*0.5)*(n+2-i))
 sinplot()
